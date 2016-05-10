a4574cc28648c474b4e6290ca2e0ffe632b5caf7
EricTing <yunding.whu@gmail.com>
2016-05-10 10:15:59 -0500

    collect distances profile
    	modified:   src/reduce.py

--- src/reduce.py
+++ src/reduce.py
@@ -1,12 +1,34 @@
 #!/usr/bin/env python
 
+import luigi
+import json
 import pandas as pd
 from collections import defaultdict
-from translate import BuildTokens
+from translate import BuildTokens, Distances
 from lists import REFINED_DIC
 
 
-def main():
+class Dists(luigi.Task):
+    def run(self):
+        all_dat = {}
+        for line in file("../dat/PDBbind_07.txt"):
+            myid = line.rstrip()
+            task = Distances(myid)
+            if task.complete():
+                with task.output().open('r') as ifs:
+                    dat = json.loads(ifs.read())
+                    all_dat[myid] = dat
+
+        with self.output().open('w') as ofs:
+            to_write = json.dumps(all_dat)
+            ofs.write(to_write)
+
+    def output(self):
+        ofn = "/work/jaydy/working/PDBbind_refined07-core07.distances.json"
+        return luigi.LocalTarget(ofn)
+
+
+def refined_tokens():
     refined_ids = REFINED_DIC.keys()
 
     refined_dat = defaultdict(list)
@@ -21,6 +43,10 @@ def main():
 
     refined_df = pd.DataFrame(refined_dat)
     refined_df.to_csv("../dat/PDBbind_refined07-core07.tokens.csv")
+
+
+def main():
+    luigi.build([Dists()], local_scheduler=True)
 
 
 if __name__ == '__main__':
